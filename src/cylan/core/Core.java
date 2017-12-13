3b41165a9f780f8785efa9a7d15b24b2b6fc0f2e
Phillip <rphillip.thomas@gmail.com>
2017-12-13 23:06:14 +0100

    Added First JFrame

---
+++ src/cylan/core/Core.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cylan.core;
+
+/**
+ *
+ * @author rphil
+ */
+public class Core {
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        NewJFrame frm = new NewJFrame();
+        frm.show();
+        
+    }
+    
+    
+    
+    
+}
